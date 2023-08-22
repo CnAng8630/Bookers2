9a074aeb93e7f41d5ff4944cfe9cdecb045a3c17
CnAng8630 <138589979+CnAng8630@users.noreply.github.com>
2023-08-22 11:10:45 +0000

    [Add]4-Bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
+  resources :users, only: [:show, :edit]
+  resources :books, only: [:new, :index, :show, :edit]
+  root to: 'homes#top'
   get 'homes/about'
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
